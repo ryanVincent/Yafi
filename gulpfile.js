038628c9bec8c7655a894a690564ccb52d8bd28e
ryanVincent <ryantelfordvincent@gmail.com>
2015-11-11 22:16:22 +0000

    added build files

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+const babel = require('gulp-babel');
+
+
+gulp.task('default', ['combine_src', 'minify']);
+
+gulp.task('combine_src', function() {
+  return gulp.src('./src/*.js')
+    .pipe(babel({
+          presets: ['es2015']
+      }))
+    .pipe(concat('yafi.js'))
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('minify', function(){
+  return gulp.src('./build/yafi.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./build/'));
+})
